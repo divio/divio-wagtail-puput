c1fac868620dd136419905592f0c1f4082c74d91
Daniele Procida <daniele@vurt.org>
2017-03-17 17:06:52 +0000

    Updated regex to allow dots in usernames

--- aldryn_config.py
+++ aldryn_config.py
@@ -8,4 +8,5 @@ class Form(forms.BaseForm):
         settings['ADDON_URLS'].insert(0, 'divio_wagtail_puput.urls')
         if 'aldryn-wagtail' in settings['INSTALLED_ADDONS']:
             settings['PUPUT_AS_PLUGIN'] = True
+            settings['PUPUT_USERNAME_REGEX'] = '\w.+'
         return settings
